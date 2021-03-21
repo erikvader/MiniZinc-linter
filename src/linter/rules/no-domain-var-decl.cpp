3062bce196ad56bccd71e2c97240bb8b8f8a7b9a
Erik Rimskog <erik.rimskog@gmail.com>
2021-03-21 22:48:51 +0100

    no domain var implemented using the searcher and checks for constant assignments

--- src/linter/rules/no-domain-var-decl.cpp
+++ src/linter/rules/no-domain-var-decl.cpp
@@ -1,12 +1,12 @@
 #include <linter/registry.hpp>
 #include <linter/rules.hpp>
-#include <minizinc/astiterator.hh>
+#include <linter/searcher.hpp>
 
 namespace {
 using namespace LZN;
 
 bool isNoDomainVar(const MiniZinc::VarDecl &vd) {
-  auto t = vd.type();
+  auto &t = vd.type();
   auto domain = vd.ti()->domain();
   return t.isvar() && t.st() == MiniZinc::Type::SetType::ST_PLAIN &&
          (t.bt() == MiniZinc::Type::BaseType::BT_INT ||
@@ -14,52 +14,49 @@ bool isNoDomainVar(const MiniZinc::VarDecl &vd) {
          t.dim() >= 0 && t.isPresent() && domain == nullptr;
 }
 
-class ItemVisitor : public MiniZinc::ItemVisitor {
-private:
-  class EVisitor : public MiniZinc::EVisitor {
-  private:
-    ItemVisitor &ivis;
-
-  public:
-    EVisitor(ItemVisitor &ivis) : ivis(ivis) {}
-    void vVarDecl(const MiniZinc::VarDecl &vd) {
-      if (isNoDomainVar(vd)) {
-        ivis.add(vd.loc());
-      }
-    }
-  };
-
-  EVisitor evis;
-  std::function<void(const MiniZinc::Location &loc)> add;
-
-public:
-  ItemVisitor(std::function<void(const MiniZinc::Location &loc)> add) : evis(*this), add(add) {}
-
-  void vVarDeclI(MiniZinc::VarDeclI *vdi) {
-    if (isNoDomainVar(*vdi->e())) {
-      add(vdi->e()->loc());
-    }
+// TODO: check if it is an expression that evaluates to a constant
+bool isAssignedConstant(const MiniZinc::VarDecl &vd) {
+  using ExpressionId = MiniZinc::Expression::ExpressionId;
+  if (vd.e() == nullptr)
+    return false;
+  switch (vd.e()->eid()) {
+  case ExpressionId::E_INTLIT:
+  case ExpressionId::E_FLOATLIT:
+  case ExpressionId::E_SETLIT:
+  case ExpressionId::E_BOOLLIT:
+  case ExpressionId::E_STRINGLIT:
+  case ExpressionId::E_ARRAYLIT: return true;
+  default: return false;
   }
-  void vAssignI(MiniZinc::AssignI *ai) { top_down(evis, ai->e()); }
-  void vConstraintI(MiniZinc::ConstraintI *ci) { top_down(evis, ci->e()); }
-  void vFunctionI(MiniZinc::FunctionI *fi) { top_down(evis, fi->e()); }
-};
+}
 
 class NoDomainVarDecl : public LintRule {
 public:
   constexpr NoDomainVarDecl() : LintRule(13, "unbounded-variable") {}
 
 private:
+  using ExpressionId = MiniZinc::Expression::ExpressionId;
+
   virtual void do_run(const MiniZinc::Model *model,
                       std::vector<LintResult> &results) const override {
-    auto add = [&results, t = this](const MiniZinc::Location &loc) {
-      results.emplace_back(
-          loc.filename().c_str(), t, "no explicit domain on variable declaration",
-          LintResult::OneLineMarked{loc.firstLine(), loc.firstColumn(), loc.lastColumn()});
-    };
-    ItemVisitor ivis(add);
-    // NOTE: assumes iter_items doesn't modify the model
-    MiniZinc::iter_items(ivis, const_cast<MiniZinc::Model *>(model));
+    auto s = SearchBuilder()
+                 .in_vardecl()
+                 .in_assign_rhs()
+                 .in_constraint()
+                 .in_function_body()
+                 .under(ExpressionId::E_VARDECL)
+                 .capture()
+                 .build();
+
+    s.search(model).for_each([&results, t = this](const Search::ModelSearcher &ms) {
+      auto &vd = *ms.capture(0)->cast<MiniZinc::VarDecl>();
+      if (isNoDomainVar(vd) && !isAssignedConstant(vd)) {
+        auto &loc = vd.loc();
+        results.emplace_back(
+            loc.filename().c_str(), t, "no explicit domain on variable declaration",
+            LintResult::OneLineMarked{loc.firstLine(), loc.firstColumn(), loc.lastColumn()});
+      }
+    });
   }
 };
 
--- tests/no-domain-var-decl.test.cpp
+++ tests/no-domain-var-decl.test.cpp
@@ -29,9 +29,21 @@ TEST_CASE("no domain on variable declarations", "[rule]") {
     LZN_EXPECTED();
   }
 
+  SECTION("assigned variable to another variable") {
+    LZN_MODEL("var int: x;\n"
+              "var int: y = x;\n");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 10), LZN_ONELINE(2, 1, 10));
+  }
+
   SECTION("assigned variable out of line") {
     LZN_MODEL("var int: x;\n"
               "x = 5;");
+    LZN_EXPECTED();
+  }
+
+  SECTION("assigned variable in constraint") {
+    LZN_MODEL("var int: x;\n"
+              "constraint x = 5;");
     LZN_EXPECTED();
   }
 
@@ -58,6 +70,11 @@ TEST_CASE("no domain on variable declarations", "[rule]") {
               "    var float : g ::bounds;\n"
               "} in g mod 2 = 0;\n");
     LZN_EXPECTED(LZN_ONELINE(2, 5, 17));
+  }
+
+  SECTION("function parameters") {
+    LZN_MODEL("function var int: asd(var int: x) = x;");
+    LZN_EXPECTED();
   }
 
   LZN_TEST_CASE_END
