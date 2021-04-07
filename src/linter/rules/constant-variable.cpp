a10201bfe6f72df952d487af9f627ca5e546122b
Erik Rimskog <erik.rimskog@gmail.com>
2021-04-07 20:25:17 +0200

    constant variable tests

--- src/linter/rules/constant-variable.cpp
+++ src/linter/rules/constant-variable.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <linter/registry.hpp>
 #include <linter/rules.hpp>
 #include <linter/searcher.hpp>
@@ -18,12 +19,35 @@ private:
       if ((rhs = vd->e()) == nullptr)
         rhs = env.get_equal_constrained_rhs(vd);
 
-      // TODO: what if array that doesn't cover all values?
       if (rhs != nullptr && vd->type().isvar() && rhs->type().isPar()) {
         auto &loc = vd->loc();
-        env.add_result(
+        env.emplace_result(
             loc.filename().c_str(), this, "is only assigned to par values, shouldn't be var",
             LintResult::OneLineMarked{loc.firstLine(), loc.firstColumn(), loc.lastColumn()});
+
+      } else if (rhs == nullptr && vd->ti()->isarray() && env.is_every_index_touched(vd)) {
+        std::vector<const MiniZinc::Location *> sub_locations;
+        auto [first, last] = env.array_equal_constrained().equal_range(vd);
+        bool all_par = std::all_of(first, last, [&sub_locations](auto &pair) {
+          auto [arrayaccess, rhs, comp] = pair.second;
+          sub_locations.push_back(&arrayaccess->loc());
+          return rhs->type().isPar();
+        });
+
+        if (all_par) {
+          auto &loc = vd->loc();
+          LintResult lr(
+              loc.filename().c_str(), this, "is only constrained to par values, shouldn't be var",
+              LintResult::OneLineMarked{loc.firstLine(), loc.firstColumn(), loc.lastColumn()});
+
+          for (auto sloc : sub_locations) {
+            lr.emplace_subresult(sloc->filename().c_str(), this, "constrained here",
+                                 LintResult::OneLineMarked{sloc->firstLine(), sloc->firstColumn(),
+                                                           sloc->lastColumn()});
+          }
+
+          env.add_result(std::move(lr));
+        }
       }
     }
   }
---
+++ tests/constant-variable.test.cpp
@@ -0,0 +1,68 @@
+#include "test_common.hpp"
+
+TEST_CASE("variable assigned to par", "[rule]") {
+  LZN_TEST_CASE_INIT(4);
+
+  SECTION("one variable not assigned") {
+    LZN_MODEL("var int: x;");
+    LZN_EXPECTED();
+  }
+
+  SECTION("one parameter not assigned") {
+    LZN_MODEL("int: x;");
+    LZN_EXPECTED();
+  }
+
+  SECTION("one parameter assigned") {
+    LZN_MODEL("int: x = 3;");
+    LZN_EXPECTED();
+  }
+
+  SECTION("one variable assigned to par") {
+    LZN_MODEL("var int: x = 4;");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 10));
+  }
+
+  SECTION("one variable constrained to par") {
+    LZN_MODEL("var int: x;\n"
+              "constraint x = 2;");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 10));
+  }
+
+  SECTION("one variable assigned out of place") {
+    LZN_MODEL("var int: x;\n"
+              "x = 2;");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 10));
+  }
+
+  SECTION("one variable assigned variable") {
+    LZN_MODEL("var int: x;\n"
+              "var int: y = x;");
+    LZN_EXPECTED();
+  }
+
+  SECTION("array assigned to par") {
+    LZN_MODEL("array[1..3] of var int: arr = [1,2,3];");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 27));
+  }
+
+  SECTION("array constrained equal in forall to par") {
+    LZN_MODEL("array[1..3] of var int: arr;\n"
+              "constraint forall(i in 1..3)(arr[i] = i*2);");
+    LZN_EXPECTED(LZN_ONELINE(1, 1, 27));
+  }
+
+  SECTION("array constrained in forall") {
+    LZN_MODEL("array[1..3] of var int: arr;\n"
+              "constraint forall(i in 1..3)(arr[i] > 0);");
+    LZN_EXPECTED();
+  }
+
+  SECTION("array constrained equal in forall to var") {
+    LZN_MODEL("array[1..3] of var int: arr;\n"
+              "constraint forall(i in 1..3)(arr[i] = arr[i]);");
+    LZN_EXPECTED();
+  }
+
+  LZN_TEST_CASE_END
+}
