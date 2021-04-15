15560599831be2e1988bf6effebaa17955d3ce8b
Erik Rimskog <erik.rimskog@gmail.com>
2021-04-15 15:09:08 +0200

    explicit element predicate

---
+++ src/linter/rules/element-predicate.cpp
@@ -0,0 +1,38 @@
+#include <linter/registry.hpp>
+#include <linter/rules.hpp>
+
+namespace {
+using namespace LZN;
+
+class ElementPredicate : public LintRule {
+public:
+  constexpr ElementPredicate() : LintRule(15, "element-predicate") {}
+
+private:
+  using ExpressionId = MiniZinc::Expression::ExpressionId;
+  using BT = MiniZinc::BinOpType;
+
+  virtual void do_run(LintEnv &env) const override {
+    const auto s = env.get_builder().in_everywhere().under(ExpressionId::E_CALL).capture().build();
+    auto ms = s.search(env.model());
+
+    while (ms.next()) {
+      auto call = ms.capture_cast<MiniZinc::Call>(0);
+      if (call->id() == MiniZinc::constants().ids.element && call->argCount() == 3) {
+        MiniZinc::GCLock lock;
+        auto arracc = new MiniZinc::ArrayAccess(MiniZinc::Location().introduce(), call->arg(1),
+                                                {call->arg(0)});
+        auto rewrite =
+            new MiniZinc::BinOp(MiniZinc::Location().introduce(), arracc, BT::BOT_EQ, call->arg(2));
+
+        const auto &loc = call->loc();
+        env.emplace_result(loc.filename().c_str(), this, "hard to read array access",
+                           FileContents::OneLineMarked(loc), rewrite);
+      }
+    }
+  }
+};
+
+} // namespace
+
+REGISTER_RULE(ElementPredicate)
