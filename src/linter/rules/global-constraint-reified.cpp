aa27b1cb0d7c4e0419a379ecf2a063fbb0f1f672
Erik Rimskog <erik.rimskog@gmail.com>
2021-04-20 00:02:02 +0200

    global constraint reified

---
+++ src/linter/rules/global-constraint-reified.cpp
@@ -0,0 +1,42 @@
+#include <linter/file_utils.hpp>
+#include <linter/registry.hpp>
+#include <linter/rules.hpp>
+
+namespace {
+using namespace LZN;
+
+class GlobalConstraintReified : public LintRule {
+public:
+  constexpr GlobalConstraintReified() : LintRule(17, "global-reified") {}
+
+private:
+  using ExpressionId = MiniZinc::Expression::ExpressionId;
+
+  virtual void do_run(LintEnv &env) const override {
+    const auto s = env.get_builder().under(ExpressionId::E_CALL).capture().build();
+
+    for (auto con : env.constraints()) {
+      auto ms = s.search(con);
+
+      while (ms.next()) {
+        auto call = ms.capture_cast<MiniZinc::Call>(0);
+        auto decl = call->decl();
+        if (decl == nullptr)
+          continue;
+
+        auto decl_path = decl->loc().filename();
+        if (s.include_path() != nullptr && decl_path.size() > 0 &&
+            path_included_from(*s.include_path(), decl_path) && decl->ti()->type().isvarbool() &&
+            con != call) {
+          const auto &loc = call->loc();
+          env.emplace_result(loc.filename().c_str(), this, "reified global constraint",
+                             FileContents::OneLineMarked(loc));
+        }
+      }
+    }
+  }
+};
+
+} // namespace
+
+REGISTER_RULE(GlobalConstraintReified)
---
+++ tests/global-constraint-reified.test.cpp
@@ -0,0 +1,36 @@
+#include "test_common.hpp"
+
+TEST_CASE("global constraint reified", "[rule]") {
+  LZN_TEST_CASE_INIT(17);
+
+  SECTION("okay alldifferent") {
+    LZN_MODEL("include \"globals.mzn\";\n"
+              "array[1..5] of var int: xs;\n"
+              "constraint alldifferent(xs);");
+    LZN_EXPECTED();
+  }
+
+  SECTION("bad alldifferent") {
+    LZN_MODEL("include \"globals.mzn\";\n"
+              "array[1..5] of var int: xs;\n"
+              "constraint alldifferent(xs) \\/ alldifferent(xs);");
+    LZN_EXPECTED(LZN_ONELINE(3, 12, 27), LZN_ONELINE(3, 32, 47));
+  }
+
+  SECTION("assigned count") {
+    LZN_MODEL("include \"globals.mzn\";\n"
+              "array[1..5] of var int: xs;\n"
+              "constraint 1 = count(xs, 1);");
+    LZN_EXPECTED();
+  }
+
+  SECTION("user defined predicate") {
+    LZN_MODEL("include \"globals.mzn\";\n"
+              "predicate f(var int: x) = x = 1;\n"
+              "array[1..5] of var int: xs;\n"
+              "constraint f(xs[2]) \\/ f(xs[1])");
+    LZN_EXPECTED();
+  }
+
+  LZN_TEST_CASE_END
+}
